b2033bc13ca831891c4370c25b3e5cd82c7132ad
Aaron Wolfcale <aaronwolfcale@Aarons-MacBook-Pro.local>
2018-03-06 11:43:20 -0600

    Flag

---
+++ Third-Party-Modules/flag.js
@@ -0,0 +1,31 @@
+#!/usr/bin/env node
+const chalk = require('chalk');
+
+let blueRedStripe = chalk.bgBlue(' * * * * * * * ') + chalk.bgRed('                                   ');
+let blueWhiteStripe = chalk.bgBlue('  * * * * * *  ') + chalk.bgWhite('                                   ');
+let redStripe = chalk.bgRed('                                                  ')
+let whiteStripe = chalk.bgWhite('                                                  ')
+
+let printFlag = () => {
+console.log(blueRedStripe);
+console.log(blueWhiteStripe);
+console.log(blueRedStripe);
+console.log(blueWhiteStripe);
+console.log(blueRedStripe);
+console.log(blueWhiteStripe);
+console.log(blueRedStripe);
+console.log(whiteStripe);
+console.log(redStripe);
+console.log(whiteStripe);
+console.log(redStripe);
+console.log(whiteStripe);
+console.log(redStripe);
+console.log(whiteStripe);
+console.log(redStripe);
+};
+
+printFlag();
+
+
+
+
