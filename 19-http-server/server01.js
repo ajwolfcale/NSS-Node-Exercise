271bd3a578574b3c083922fbbaaa6cd15e1ec24e
Aaron Wolfcale <ajwolfcale@gmail.com>
2018-03-15 11:29:03 -0500

    http-server exercise

---
+++ 19-http-server/server01.js
@@ -0,0 +1,20 @@
+const http = require('http');
+const server  = http.createServer();
+const { readFile } = require('fs');
+const { createServer }  = require('http');
+
+server.on('request',(req, res) => {
+    // let url = req.url;
+    let resource = 'index.html';
+    readFile(resource, (err, file) => {
+        if(err){
+            res.statusCode = 404;
+            return res.end('<h1>Not found, doood!<h1>');
+        }
+        res.end(file);
+    });
+});
+
+server.listen(8080, () => {
+    console.log('listening on port 8080');
+});
